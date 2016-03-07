19dfeb90d8b99a69f5a22562201b84aaf848afba
jsetyobudi <jsetyobudi@gmail.com>
2016-03-07 11:06:00 -0500

    Create Ingredient.java

---
+++ Ingredient.java
@@ -0,0 +1,48 @@
+/**
+ * Tag class 
+ * Potluck recipe project
+ * CST 8288
+ * March 7, 2016
+ * @author Johan Setyobudi
+ *
+ */
+public class Ingredient {
+	
+	private static int id = 0;
+	private String name;
+	private String measurement;
+	
+	public Ingredient() {
+		name = new String();
+		measurement = new String();
+	}
+	
+	public Ingredient(String name, String measurement) {
+		id = id++;
+		this.name = name;
+		this.measurement = measurement;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getMeasurement() {
+		return measurement;
+	}
+
+	public void setMeasurement(String measurement) {
+		this.measurement = measurement;
+	}
+	
+	
+
+}
