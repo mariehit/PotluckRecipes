eaac2a6fba988e0f2fcf61dc51cdf9a9f178a961
jsetyobudi <jsetyobudi@gmail.com>
2016-03-07 11:05:32 -0500

    Create Tag.java

---
+++ Tag.java
@@ -0,0 +1,33 @@
+/**
+ * Tag class 
+ * Potluck recipe project
+ * CST 8288
+ * March 7, 2016
+ * @author Johan Setyobudi
+ *
+ */
+public class Tag {
+	
+	private static int id = 0;
+	private String text;
+	
+	public Tag() {
+		text = new String();
+	}
+	
+	public Tag(int id, String text) {
+		id = id++;
+		this.text = text;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	
+	public String getText() {
+		return text;
+	}
+	public void setText(String text) {
+		this.text = text;
+	}
+}
