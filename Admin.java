45db5c22fe048fa02c426d5aad930fa2652a122c
jaketfield <jaketfield@gmail.com>
2016-03-10 20:06:59 -0500

    Added files via upload

---
+++ Admin.java
@@ -0,0 +1,33 @@
+/**
+ *  Admin class
+ *  CST8288
+ * @author jacob
+ *	March 10/2016
+ */
+public class Admin implements MemberInfo{
+	
+	public void comment(){
+		
+	}
+	
+	public void createRecipe(){
+		
+	}
+	
+	public void deleteComment() {
+		
+	}
+	
+	public void viewComment(){
+		
+	}
+	
+	public void viewRecipe(){
+		
+	}
+	
+	public String accountBehaviour(){
+		
+		return "Admin";
+	}
+}
