d4771c8549270b2368132582c0f54aed2ba62cf2
mariehit <mariehit@gmail.com>
2016-03-14 10:31:18 -0400

    Create Category.java

---
+++ Category.java
@@ -0,0 +1,16 @@
+
+public class Category {
+	private int category_id;
+	private CategoryName categoryName;
+	
+	//
+	public CategoryName getCategoryName() {
+		return categoryName;
+	}
+	public void setCategoryName(CategoryName categoryName) {
+		this.categoryName = categoryName;
+	}
+	public int getCategory_id() {
+		return category_id;
+	}
+}
