d2245e39deb4676d8f40ed35e83fd4b602b3bad4
mariehit <mariehit@gmail.com>
2016-04-04 12:56:59 -0400

    Update DataSource.java

--- Project2PotLuckRecipes/src/java/dataaccesslayer/DataSource.java
+++ Project2PotLuckRecipes/src/java/dataaccesslayer/DataSource.java
@@ -5,10 +5,33 @@
  */
 package dataaccesslayer;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
 /**
  *
- * @author Johan
+ * @author Marie
  */
 public class DataSource {
+    
+    public DataSource(){}
+
+	public Connection createConnection(){
+		Connection connection = null;
+            try{
+                Context initCtx = new InitialContext();
+                Context envCtx = (Context)initCtx.lookup("java:comp/env");
+                javax.sql.DataSource ds = (javax.sql.DataSource)envCtx.lookup("jdbc/potluckrecipes");
+                connection = ds.getConnection();
+            }
+            catch(NamingException | SQLException ex){
+                ex.printStackTrace();
+            }
+            return connection;
+
+	}
     
 }
