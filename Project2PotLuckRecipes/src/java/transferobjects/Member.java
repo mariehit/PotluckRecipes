258559e9f152241c3d7eb90f2834035b0d2b3802
linrez <lin00096@algonquinlive.com>
2016-04-15 22:57:50 -0400

    Added files via upload

--- Project2PotLuckRecipes/src/java/transferobjects/Member.java
+++ Project2PotLuckRecipes/src/java/transferobjects/Member.java
@@ -9,6 +9,70 @@ package transferobjects;
  *
  * @author Johan
  */
-public class Member {
+public class Member 
+{
+    private int memberId;
+    private boolean isAdmin;
+    private String Alias;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String username;
+    private String password;
+    private String Salt;
     
+    public int getMemberId() {
+		return memberId;
+	}
+	public void setMemberId(int memberId) {
+		this.memberId = memberId;
+	}
+	public boolean isAdmin() {
+		return isAdmin;
+	}
+	public void setAdmin(boolean isAdmin) {
+		this.isAdmin = isAdmin;
+	}
+	public String getAlias() {
+		return Alias;
+	}
+	public void setAlias(String alias) {
+		Alias = alias;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getSalt() {
+		return Salt;
+	}
+	public void setSalt(String salt) {
+		Salt = salt;
+	}
 }
